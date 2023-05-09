1a2cc23674e23ca4a40da6ce5268c9154435479a
AlexWang233 <alex13901350@gmail.com>
2023-05-09 02:21:42 -0400

    Create LC208.py

---
+++ LC208.py
@@ -0,0 +1,35 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.endNode = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+        cur.endNode = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                return False
+            cur = cur.children[w]
+        return cur.endNode
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for w in prefix:
+            if w not in cur.children:
+                return False
+            cur = cur.children[w]
+        return True

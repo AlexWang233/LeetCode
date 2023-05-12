9ba254343be5dfd9c164c73ca5e8932f99dc3d9b
AlexWang233 <alex13901350@gmail.com>
2023-05-12 12:25:10 -0400

    Create LC1372.py

---
+++ LC1372.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+
+        def dfs(node, left, right):
+            self.ans = max(self.ans, left + right)
+
+            if node.left:
+                dfs(node.left, right + 1, 0)
+
+            if node.right:
+                dfs(node.right, 0, left + 1)
+
+        dfs(root, 0, 0)
+
+        return self.ans

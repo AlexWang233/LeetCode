a4a4c96f3730d73a430629f1366e5eb4648a4656
AlexWang233 <alex13901350@gmail.com>
2023-05-18 19:33:46 -0400

    Update LC373.py

--- LC373.py
+++ LC373.py
@@ -4,12 +4,13 @@ class Solution:
         hq = []
         l1 = len(nums1)
         l2 = len(nums2)
-        k = min(k, l1 * l2)
-        for i in range(k):
+        for i in range(min(k, l1)):
             heapq.heappush(hq, (nums1[i] + nums2[0], nums1[i], nums2[0], 0))
         for _ in range(k):
             temp, n, m, i = heapq.heappop(hq)
             res.append([n, m])
             if i + 1 < l2:
                 heapq.heappush(hq, (n + nums2[i + 1], n, nums2[i+1], i+1))
+            if not hq:
+                break
         return res

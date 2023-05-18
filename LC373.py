9c7b58a92fd48546923b2b8d0f4b26b04126c70c
AlexWang233 <alex13901350@gmail.com>
2023-05-18 19:32:40 -0400

    Create LC373.py

---
+++ LC373.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        res = []
+        hq = []
+        l1 = len(nums1)
+        l2 = len(nums2)
+        k = min(k, l1 * l2)
+        for i in range(k):
+            heapq.heappush(hq, (nums1[i] + nums2[0], nums1[i], nums2[0], 0))
+        for _ in range(k):
+            temp, n, m, i = heapq.heappop(hq)
+            res.append([n, m])
+            if i + 1 < l2:
+                heapq.heappush(hq, (n + nums2[i + 1], n, nums2[i+1], i+1))
+        return res

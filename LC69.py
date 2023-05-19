7c5539e6b600d3f2f1a617b1b99824147d894d03
AlexWang233 <alex13901350@gmail.com>
2023-05-19 14:52:38 -0400

    Update LC69.py

--- LC69.py
+++ LC69.py
@@ -2,5 +2,5 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         res = x
         while res * res > x:
-            res = (res + x / res) / 2
+            res = (res + x / res) // 2
         return int(res)

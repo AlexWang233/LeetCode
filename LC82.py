215131ab082857f69c12093631342774f163caca
AlexWang233 <alex13901350@gmail.com>
2023-05-24 22:18:23 -0400

    Create LC82.py

---
+++ LC82.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ans = node = ListNode(0)
+        ans.next = head
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                node.next = head
+            else:
+                node = node.next
+                head = head.next
+        return ans.next

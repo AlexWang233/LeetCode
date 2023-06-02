dd429730eb9dd7d7f440e81a0db86b3c3c4c8aa9
AlexWang233 <alex13901350@gmail.com>
2023-06-02 15:55:52 -0400

    Create LC1581.sql

---
+++ SQL/LC1581.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT customer_id, COUNT(v.visit_id) as count_no_trans FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE amount IS NULL GROUP BY customer_id

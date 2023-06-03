3001829e0b49c8a9b4c4288693d86bba37e5bceb
AlexWang233 <alex13901350@gmail.com>
2023-06-03 01:34:13 -0400

    Create LC1193.sql

---
+++ SQL/LC1193.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(id) AS trans_count,
+SUM(state = 'approved') AS approved_count, SUM(amount) AS trans_total_amount,
+SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions GROUP BY month, country

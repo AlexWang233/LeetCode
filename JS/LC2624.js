ab21a9880f4c82510c3df462a0881511fb5222cd
AlexWang233 <alex13901350@gmail.com>
2023-06-06 22:50:00 -0400

    Create LC2624.js

---
+++ JS/LC2624.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function (rowsCount, colsCount) {
+  if (rowsCount * colsCount != this.length) {
+    return [];
+  }
+  let res = Array(rowsCount)
+    .fill()
+    .map(() => []);
+  // console.log(res);
+  for (let i = 0; i < this.length; i++) {
+    if (j % 2 == 0) {
+      res[i % rowsCount].push(this[i]);
+    } else {
+      res[rowsCount - (i % rowsCount) - 1].push(this[i]);
+    }
+  }
+  return res;
+};
+
+/**
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */

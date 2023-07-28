1daf954852a79760fa4c28be84d5783144c6e82f
AlexWang233 <alex13901350@gmail.com>
2023-07-27 22:17:00 -0400

    Update to make code a bit more concise

--- Python/LC486.py
+++ Python/LC486.py
@@ -3,12 +3,9 @@ class Solution:
         if len(nums) % 2 == 0:
             return True
         l = len(nums)
-        dp = [0] * l
+        dp = [0] * (l + 1)
         for i in range(l):
             for j in range(i, -1, -1):
-                # here dp[k] represents max net value of player 1 if we use only indices j to i
-                if i == j:
-                    dp[i] = nums[j]
-                else:
-                    dp[j] = max(nums[i] - dp[j], nums[j] - dp[j+1])
+                # here dp[j] represents max net value of player 1 if we use only indices j to i
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j+1])
         return dp[0] >= 0

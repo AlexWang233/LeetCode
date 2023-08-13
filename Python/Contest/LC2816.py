852ed8e9a828a0558181d2715c4b9074b59f7079
AlexWang233 <alex13901350@gmail.com>
2023-08-13 07:33:45 -0400

    Daily Problem

---
+++ Python/Contest/LC2816.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.val * 2 >= 10:
+            head = ListNode(0, head)
+        node = head
+        while node:
+            node.val *= 2
+            if node.next and node.next.val * 2 >= 10:
+                node.val += 1
+            node.val %= 10
+            node = node.next
+        return head

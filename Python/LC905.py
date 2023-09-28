d9dbf828538ed9ad1053ec65d8852306d3fccee5
AlexWang233 <alex13901350@gmail.com>
2023-09-27 21:57:13 -0400

    Daily Problem

---
+++ Python/LC905.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        j = 0
+        for i, n in enumerate(nums):
+            if n % 2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+        return nums
+
+            

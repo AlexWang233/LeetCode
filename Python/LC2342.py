d3c4a5adb830c059ae16f5978248161e1ee68f77
AlexWang233 <alex13901350@gmail.com>
2025-02-11 23:29:32 -0800

    Update

--- Python/LC2342.py
+++ Python/LC2342.py
@@ -2,7 +2,6 @@ class Solution:
     def maximumSum(self, nums: List[int]) -> int:
         m = defaultdict(lambda: 0)
         ans = -1
-        d = defaultdict(lambda: -1)
         def sd(n):
             res = 0
             while n:
@@ -12,9 +11,8 @@ class Solution:
         for n in nums:
             s = sd(n)
             if s in m:
-                d[s] = max(d[s], m[s] + n)
+                ans = max(ans, m[s] + n)
                 m[s] = max(m[s], n)
-                ans = max(ans, d[s])
             else:
                 m[s] = n
         return ans

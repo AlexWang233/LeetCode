b47349f8e460bdbd05787ee61c112a1c7442f518
AlexWang233 <alex13901350@gmail.com>
2025-03-05 22:26:14 -0800

    Fix

--- Python/LC2965.py
+++ Python/LC2965.py
@@ -1,13 +1,13 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         visited = set([])
-        res = set([])
+        dup = -1
         n = len(grid)
+        s = set([(i + 1) for i in range(n * n)])
         for row in grid:
             for i in row:
-                if i in visited:
-                    res.add(i)
+                if dup < 0 and i in visited:
+                    dup = i
                 visited.add(i)
         
-        s = set([(i + 1) for i in range(n * n)])
-        return list(res.union(s - visited))
+        return [dup] + list(s - visited)

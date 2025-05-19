ab75b2ceb25abacf091d17a854ee566cc9fdee9d
AlexWang233 <alex13901350@gmail.com>
2025-05-19 02:11:04 -0700

    Daily Problem

---
+++ Python/LC3024.py
@@ -0,0 +1,10 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        nums.sort()
+        if nums[2] >= nums[0] + nums[1]:
+            return "none"
+        if nums[0] == nums[1] == nums[2]:
+            return "equilateral"
+        if nums[0] == nums[1] or nums[1] == nums[2]:
+            return "isosceles"
+        return "scalene"

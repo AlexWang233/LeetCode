1b445aa0f0f72b4946672c05cb69b88853c2d5ed
AlexWang233 <alex13901350@gmail.com>
2025-08-12 18:32:42 -0700

    Daily SQL

---
+++ Python/LC1587.py
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT name, SUM(amount) AS balance
+FROM Users u
+LEFT JOIN Transactions t
+ON u.account = t.account
+GROUP BY u.account
+HAVING SUM(amount) > 10000
